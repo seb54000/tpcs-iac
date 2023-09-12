c6b24e6e759c37cb7771f755a708f582e59df9a3
seb54000 <seb54000@gmail.com>
2023-09-12 04:37:47 +0200

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # tp-centralesupelec-iac
 New iac tp (azure and vikunja)
+
+
+This exercices are designed to be deployed by Terraform and ansible
+
+For the terraform part, we could envisage 2 cloud provider
+- AZURE (to have a new one)
+- AWS
+
+# AZURE
+
+Using a microsoft online account associated with sebastien.claude@centralesupelec.fr
+- not working, we have to use personnal account (as company account doesn't allow access to AD)
+  - so using seabstien.claude@multiseb.com
+
+https://portal.azure.com/
+
+https://developer.hashicorp.com/terraform/tutorials/azure-get-started/azure-build
+
+brew update && brew install azure-cli
+az login
+az account set --subscription "xxxxxxxxxx" # Note the subscription is the id (not the tenantid)
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/xxxxxxxxxx"
+
+
+
+```
+export ARM_CLIENT_ID="<APPID_VALUE>"
+export ARM_CLIENT_SECRET="<PASSWORD_VALUE>"
+export ARM_SUBSCRIPTION_ID="<SUBSCRIPTION_ID>"
+export ARM_TENANT_ID="<TENANT_VALUE>"
+```
+
+https://terraformguru.com/terraform-real-world-on-azure-cloud/10-Azure-Virtual-Network-4Tier/
+https://www.dotnettricks.com/learn/azure/what-is-microsoft-azure-virtual-network-and-architecture
---
+++ terraform/azure/main.tf
@@ -0,0 +1,160 @@
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tp-iac-rg"
+  location = "francecentral"
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+#   name                = "${local.resource_name_prefix}-${var.vnet_name}"
+#   address_space       = var.vnet_address_space
+  name                = "vpc"
+  address_space = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+#   tags = local.common_tags
+}
+
+resource "azurerm_subnet" "front" {
+  name                 = "front-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "api" {
+  name                 = "api-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "db" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+
+
+
+
+resource "azurerm_public_ip" "web_linuxvm_publicip" {
+  name                = "web-linuxvm-publicip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku = "Standard"
+  # https://learn.microsoft.com/en-us/azure/virtual-network/ip-services/public-ip-basic-upgrade-guidance#basic-sku-vs-standard-sku
+}
+
+resource "azurerm_network_interface" "web_linuxvm_nic" {
+  name                = "web-linuxvm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "web-linuxvm-ip-1"
+    subnet_id                     = azurerm_subnet.front.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.web_linuxvm_publicip.id 
+  }
+}
+
+resource "azurerm_network_security_group" "web_vmnic_nsg" {
+  name                = "${azurerm_network_interface.web_linuxvm_nic.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "web_vmnic_nsg_associate" {
+  depends_on = [ azurerm_network_security_rule.web_vmnic_nsg_rule_inbound]
+  network_interface_id      = azurerm_network_interface.web_linuxvm_nic.id
+  network_security_group_id = azurerm_network_security_group.web_vmnic_nsg.id
+}
+
+## Locals Block for Security Rules
+locals {
+  web_vmnic_inbound_ports_map = {
+    "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
+    "110" : "443",
+    "120" : "22"
+  } 
+}
+## NSG Inbound Rule for WebTier Subnets
+resource "azurerm_network_security_rule" "web_vmnic_nsg_rule_inbound" {
+  for_each = local.web_vmnic_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value 
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.web_vmnic_nsg.name
+}
+
+# Locals Block for custom data
+locals {
+webvm_custom_data = <<CUSTOM_DATA
+#!/bin/sh
+#!/bin/sh
+#sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo chmod -R 777 /var/www/html 
+sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo mkdir /var/www/html/app1
+sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
+sudo echo "Welcome to stacksimplify - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
+CUSTOM_DATA  
+}
+
+
+# Resource: Azure Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "web_linuxvm" {
+  name = "web-linuxvm"
+  #computer_name = "web-linux-vm"  # Hostname of the VM (Optional)
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  size = "Standard_DS1_v2"
+  admin_username = "azureuser"
+  network_interface_ids = [ azurerm_network_interface.web_linuxvm_nic.id ]
+  admin_ssh_key {
+    username = "azureuser"
+    public_key = file("${path.module}/ssh-keys/terraform-azure.pem.pub")
+  }
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "RedHat"
+    offer = "RHEL"
+    sku = "83-gen2"
+    version = "latest"
+  }
+  #custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")    
+  custom_data = base64encode(local.webvm_custom_data)  
+
+}
+
+
+output "web_linuxvm_public_ip" {
+  description = "Web Linux VM Public Address"
+  value = azurerm_public_ip.web_linuxvm_publicip.ip_address
+}
+
+output "web_linuxvm_public_ip_address" {
+  description = "Web Linux Virtual Machine Public IP"
+  value = azurerm_linux_virtual_machine.web_linuxvm.public_ip_address
+}
