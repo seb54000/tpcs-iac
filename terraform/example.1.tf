3fd90dc4b816a149fbbfc2a14f081b5f8783ec79
seb54000 <32299345+seb54000@users.noreply.github.com>
2024-06-13 14:11:31 +0200

    t2.small instance types are not available on new regions

--- terraform/example.1.tf
+++ terraform/example.1.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test-instance" {
   ami             = "ami-0a7899f097e6c2e90"   # https://cloud-images.ubuntu.com/locator/ec2/
-  instance_type = "t2.small"
+  instance_type = "t3.small"
 
   tags = {
     filter = chomp(file("/etc/hostname"))
