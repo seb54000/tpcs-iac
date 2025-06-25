ab4eac945a363f77da49ab4bb5a2ce560f75ea17
seb54000 <seb54000@gmail.com>
2025-06-25 11:37:27 +0200

    micro instance-type for TF

--- terraform/example.1.tf
+++ terraform/example.1.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "test-instance" {
   ami             = "<AMI-ID-for-your-region>"   # https://cloud-images.ubuntu.com/locator/ec2/ : Arch amd64 , Jammy or Noble
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
 
   tags = {
     filter = chomp(file("/etc/hostname"))
-  }  
+  }
 }
